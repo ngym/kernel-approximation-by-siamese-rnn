7519df35cf0fbf9ebe381f15d8061cc147954717
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2017-05-23 16:48:55 +0200

    construct a complete gram matrix, then make losses, eventually complete the imcomplete matrix with two matrix completion algorithms

--- gak_gram_matrix_completion/gak_gram_matrix_completion.py
+++ gak_gram_matrix_completion/gak_gram_matrix_completion.py
@@ -47,33 +47,51 @@ gram = None
 
 seqs = {}
 
-def read_mats_and_build_seqs(files):
+def read_mats_and_build_seqs(files, attribute_type):
     for f in files:
         mat = sio.loadmat(f)
-        seqs[f] = second_map(np.float64, mat['gest'].transpose())
+        seqs[f] = second_map(np.float64, pick_attribute(mat['gest'].transpose(), attribute_type))
 
+def pick_attribute(ll, attribute_type):
+    retval = []
+    if attribute_type == "position":
+        for l in ll:
+            retval.append(l[1:4])
+    elif attribute_type == "velocity":
+        for i in range(ll.__len__() - 1):
+            retval.append([(ll[i+1][1] - ll[i][1])/(ll[i+1][0] - ll[i][0]),
+                           (ll[i+1][2] - ll[i][2])/(ll[i+1][0] - ll[i][0]),
+                           (ll[i+1][3] - ll[i][3])/(ll[i+1][0] - ll[i][0])])
+    elif attribute_type == "acceleration":
+        for l in ll:
+            retval.append(l[8:11])
+    elif attribute_type == "angular velocity":
+        for l in ll:
+            retval.append(l[11:14])
+    elif attribute_type == "orientation":
+        for l in ll:
+            retval.append(l[4:8])
+    else:
+        print("attribute type error.")
+        assert False
+    return retval
+        
 def second_map(func, ll):
     retval = []
     for l in ll:
-        retval.append(list(map(func, l[1:4])))
+        retval.append(list(map(func, l)))
     return np.array(retval)
 
-DATA_DIR = "/Users/ngym/Lorincz-Lab/project/fast_time-series_data_classification/dataset/6DMG_mat_112712/matR_char/"
-
-def gak(seq1, seq2):
+def gak(seq1, seq2, sigma):
     #print(threading.get_ident())
     if seq1 is seq2:
         return 1
-    if matrix_completion != "NO_COMPLETION":
-        randval = random.randint(0, 9)
-        if randval == 0:
-            return np.nan
     
     T1 = seq1.__len__()
     T2 = seq2.__len__()
 
     #sigma = 0.5*(T1+T2)/2*np.sqrt((T1+T2)/2)
-    sigma = 2 ** 0
+    #sigma = 2 ** 0
     #print("sigma: " + repr(sigma), end="  ")
     
     diff_t = np.abs(T1-T2)
@@ -86,25 +104,44 @@ def gak(seq1, seq2):
         assert kval == 0
     return kval
 
-matrix_completion = "NO_COMPLETION"
+def plot(file_name, similarities, files_to_show):
+    # To fix the direction of the matrix as the diagonal line is from top-left to bottom-right.
+    similarities_ = similarities[::-1]
+    files_to_show = []
+    for f in files:
+        files_to_show.append(f.split('/')[-1].split('.')[0])
+    files_to_show_ = files_to_show[::-1]
+    
+    trace = pgo.Heatmap(z=similarities_,
+                        x=files_to_show,
+                        y=files_to_show_,
+                        zmin=0, zmax=1
+    )
+    data=[trace]
+    po.plot(data, filename=file_name, auto_open=False)
 
 if __name__ == "__main__":
     config_json_file = sys.argv[1]
     config_dict = json.load(open(config_json_file, 'r'))
     
-    file_out = config_dict['output_html']
+    html_out_no_completion = config_dict['output_html']['NO_COMPLETION']
+    html_out_nuclear_norm_minimization = config_dict['output_html']['NUCLEAR_NORM_MINIMIZATION']
+    html_out_soft_impute = config_dict['output_html']['SOFT_IMPUTE']
+    
+    mat_out_no_completion = config_dict['output_mat']['NO_COMPLETION']
+    mat_out_nuclear_norm_minimization = config_dict['output_mat']['NUCLEAR_NORM_MINIMIZATION']
+    mat_out_soft_impute = config_dict['output_mat']['SOFT_IMPUTE']
+
+    attribute_type = config_dict['data_attribute_type']
+
     num_thread = config_dict['num_thread']
     gak_logfile = config_dict['gak_logfile']
     data_files = config_dict['data_mat_files']
-    matrix_completion = config_dict['matrix_completion']
-    print(matrix_completion)
-    if matrix_completion in {"NO_COMPLETION", "NUCLEAR_NORM_MINIMIZATION", "SOFT_IMPUTE"}:
-        print(matrix_completion + " is selected")
-    else:
-        print("Invalid completion option")
-        assert False
+    gak_sigma = config_dict['gak_sigma']
+    random_seed = config_dict['random_seed']
+    incomplete_persentage = config_dict['incomplete_persentage']
 
-    random.seed(2)
+    random.seed(random_seed)
         
     gak_logger = Logger(gak_logfile)
 
@@ -117,7 +154,7 @@ if __name__ == "__main__":
         files += files_
     files = sorted(files)
 
-    read_mats_and_build_seqs(files)
+    read_mats_and_build_seqs(files, attribute_type)
     
     gram = GRAMmatrix(files)
 
@@ -126,19 +163,19 @@ if __name__ == "__main__":
 
     futures = []
 
-    def worker_for_f1(f1index, f2indices):
+    def worker_for_f1(f1index, f2indices, gak_sigma):
         f1 = files[f1index]
         seq1 = seqs[f1]
         ret_dict = {}
         for f2index in f2indices:
             f2 = files[f2index]
             seq2 = seqs[f2]
-            ret_dict[f2] = gak(seq1, seq2)
+            ret_dict[f2] = gak(seq1, seq2, gak_sigma)
         return ret_dict
     #seqs[files[f2index]]
     with concurrent.futures.ProcessPoolExecutor(max_workers=num_thread) as executor:
         print("Start submitting jobs.")
-        future_to_files = {executor.submit(worker_for_f1, f1index, range(f1index, file_num)):
+        future_to_files = {executor.submit(worker_for_f1, f1index, range(f1index, file_num), gak_sigma):
                            files[f1index]
                            for f1index in range(file_num)}
         num_futures = future_to_files.__len__()
@@ -162,36 +199,50 @@ if __name__ == "__main__":
     for i in gram.gram.values():
         similarities.append(list(i.values()))
 
-    matrix_incomplete = np.array(similarities)
+    incomplete_similarities = []
+    for s_row in similarities:
+        is_row = []
+        for s in s_row:
+            if s == 1:
+                if similarities.index(s_row) == s_row.index(s):
+                    is_row.append(1)
+                    continue
+            if random.randint(0, 99) < incomplete_persentage:
+                is_row.append(np.nan)
+            else:
+                is_row.append(s)
+        incomplete_similarities.append(is_row)
+                
+    # "NO_COMPLETION"
+    plot(html_out_no_completion,
+         similarities, files)
+    sio.savemat(mat_out_no_completion, dict(gram=similarities, indices=files))
+    
+    # "NUCLEAR_NORM_MINIMIZATION":
+    """
+    matrix completion using convex optimization to find low-rank solution
+    that still matches observed values. Slow!
+    """
+    completed_similarities = NuclearNormMinimization().complete(incomplete_similarities)
+    plot(html_out_nuclear_norm_minimization,
+         completed_similarities, files)
+    sio.savemat(mat_out_nuclear_norm_minimization, dict(gram=completed_similarities, indices=files))
 
-    if matrix_completion == "NO_COMPLETION":
-        processed_similarities = similarities
-    elif matrix_completion == "NUCLEAR_NORM_MINIMIZATION":
-        # matrix completion using convex optimization to find low-rank solution
-        # that still matches observed values. Slow!
-        X_filled = NuclearNormMinimization().complete(matrix_incomplete)
-        processed_similarities = X_filled
-    elif matrix_completion == "SOFT_IMPUTE":
-        # Instead of solving the nuclear norm objective directly, instead
-        # induce sparsity using singular value thresholding
-        X_filled = SoftImpute().complete(matrix_incomplete)
-        processed_similarities = X_filled
-    else:
-        print("Invalid completion option")
-        assert False
+    # "SOFT_IMPUTE"
+    """
+    Instead of solving the nuclear norm objective directly, instead
+    induce sparsity using singular value thresholding
+    """
+    completed_similarities = SoftImpute().complete(incomplete_similarities)
+    plot(html_out_soft_impute,
+         completed_similarities, files)
+    sio.savemat(mat_out_soft_impute, dict(gram=completed_similarities, indices=files))
+ 
 
-    # To fix the direction of the matrix as the diagonal line is from top-left to bottom-right.
-    processed_similarities_ = processed_similarities[::-1]
-    files_to_show = []
-    for f in files:
-        files_to_show.append(f.split('/')[-1])
-    files_to_show_ = files_to_show[::-1]
-        
-    trace = pgo.Heatmap(z=processed_similarities_,
-                        x=files_to_show,
-                        y=files_to_show_,
-                        zmin=0, zmax=1
-    )
-    data=[trace]
-    po.plot(data, filename=file_out, auto_open=False)
-
+    """
+    loaded_mat = sio.loadmat(mat_out_nuclear_norm_minimization)
+    print(loaded_mat['gram'])
+    mat = second_map(np.float64, loaded_mat['gram'])
+    print(mat)
+    plot("tmp.html", mat, loaded_mat['indices'])
+    """
