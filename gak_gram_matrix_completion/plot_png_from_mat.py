64aefaadc19262d0998ccf13ae818314e2094533
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2017-06-01 15:46:30 +0200

    matplotlib

--- gak_gram_matrix_completion/plot_png_from_mat.py
+++ gak_gram_matrix_completion/plot_png_from_mat.py
@@ -3,30 +3,17 @@ import numpy as np
 import scipy as sp
 from scipy import io
 
-import plotly.plotly as py
-import plotly.graph_objs as pgo
+import matplotlib.pyplot as plt
 
 def plot(file_name, similarities, files):
-    # To fix the direction of the matrix as the diagonal line is from top-left to bottom-right.
     similarities_ = similarities[::-1]
     files_to_show = []
     for f in files:
         files_to_show.append(f.split('/')[-1].split('.')[0])
     files_to_show_ = files_to_show[::-1]
-    
-    data = [pgo.Heatmap(z=similarities_,
-                        x=files_to_show,
-                        y=files_to_show_,
-                        zmin=0, zmax=1)
-            ]
-    layout = pgo.Layout(xaxis=dict(side='top',
-                                   tickfont=dict(size=5)),
-                        yaxis=dict(tickfont=dict(size=5)))
-    fig = pgo.Figure(data=data, layout=layout)
 
-    py.image.save_as(fig,
-                     filename=file_name,
-                     scale=10)
+    f = plt.imshow(similarities, interpolation='nearest')
+    plt.savefig(file_name,  format='pdf', dpi=1200)
 
 def main():
     filename = sys.argv[1]
@@ -37,7 +24,7 @@ def main():
 
     seed = 1
         
-    png_out = filename.replace(".mat", ".png")
+    png_out = filename.replace(".mat", ".pdf")
     
     # OUTPUT
     plot(png_out,
