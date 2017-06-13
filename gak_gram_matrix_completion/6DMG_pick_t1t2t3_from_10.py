6bda4bb6ecc0169885888c363ad49b947f9703a6
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2017-06-13 15:45:38 +0200

    for reducing RNN computation time, pick up only 3 trianls from 10 trials in a mat file

---
+++ gak_gram_matrix_completion/6DMG_pick_t1t2t3_from_10.py
@@ -0,0 +1,43 @@
+import sys, random, copy, os, gc, time, csv
+import os.path as path
+from collections import OrderedDict
+from tempfile import mkdtemp
+
+import numpy as np
+import scipy as sp
+from scipy import io
+from scipy.io import wavfile
+from scipy import signal
+
+def main():
+    filename = sys.argv[1]
+    mat = io.loadmat(filename)
+    similarities = mat['gram']
+    files = mat['indices']
+
+    def cond(f):
+        if f.find("t01.mat") != -1 or \
+           f.find("t02.mat") != -1 or \
+           f.find("t03.mat") != -1:
+            return True
+        else:
+            return False
+    
+    num = len(files)
+    picked_similarities = [
+        [similarities[j_row][i_column]
+         for i_column in range(num)
+         if cond(files[i_column])]
+        for j_row in range(num)
+        if cond(files[j_row])
+    ]
+    picked_files = [f for f in files if cond(f)]
+
+    pdf_out = filename.replace(".mat", "_t1-t3.pdf")
+    mat_out  = filename.replace(".mat", "_t1-t3.mat")
+
+    io.savemat(mat_out, dict(gram=picked_similarities,
+                             indices=picked_files))
+
+if __name__ == "__main__":
+    main()
