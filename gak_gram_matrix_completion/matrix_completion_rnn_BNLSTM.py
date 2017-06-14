803d82ca305253f1cbe1bbe3270690b583c19cb2
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2017-06-14 12:02:14 +0200

    work around tensorflow problem in BNLSTM

--- gak_gram_matrix_completion/matrix_completion_rnn_BNLSTM.py
+++ gak_gram_matrix_completion/matrix_completion_rnn_BNLSTM.py
@@ -48,7 +48,7 @@ def create_base_network(input_shape, mask_value):
     #seq.add(LSTM(100, kernel_regularizer=l2(0.01), return_sequences=True))
     #seq.add(Dropout(0.1))
     seq.add(BNLSTM(units=1000, kernel_regularizer=l2(0.01), recurrent_regularizer=l2(0.01),
-                 dropout=0.5, implementation=2, return_sequences=False))
+                 dropout=0.5, implementation=1, return_sequences=False))
     seq.add(Dropout(0.5))
     seq.add(Dense(300, activation='linear', kernel_regularizer=l2(0.01)))
     seq.add(BatchNormalization())
