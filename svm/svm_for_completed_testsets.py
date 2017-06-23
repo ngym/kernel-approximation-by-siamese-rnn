9ff093b9f71ba43e8369fd3bf0aca8d0bf16d38c
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2017-06-23 20:26:24 +0200

    adapt to input. drafted but not tested yet.

--- svm/svm_for_completed_testsets.py
+++ svm/svm_for_completed_testsets.py
@@ -3,14 +3,12 @@ import sys, json, glob, os
 from sklearn.svm import SVC
 from sklearn.preprocessing import LabelBinarizer
 import sklearn.metrics as metrics
-import scipy.io as sio
+import scipy.io as io
 import numpy as np
 import functools
 
 dataset_type = None
 attribute_type = None
-loss_persentage = None
-data_dir = "/Users/ngym/Lorincz-Lab/project/fast_time-series_data_classification/program/gak_gram_matrix_completion/OUTPUT/"
 
 def convert_index_to_attributes(index):
     if dataset_type == "upperChar":
@@ -35,36 +33,22 @@ def convert_index_to_attributes(index):
         assert False
     return dict(type=type_, ground_truth=ground_truth, k_group=k_group, trial=trial)
 
-def separate_gram(gram, indices_to_separate):
-    unmatched = []
-    matched = []
-    matched_is = []
-    for i in range(gram.__len__()):
-        if i in indices_to_separate:
-            matched.append(gram[i])
-            matched_is.append(i)
-        else:
-            unmatched.append(gram[i])
-    new_matched = []
-    for row in matched:
-        new_row_matched = []
-        for i in range(gram.__len__()):
-            if i not in matched_is:
-                new_row_matched.append(row[i])
-        new_matched.append(new_row_matched)
-    new_unmatched = []
-    for row in unmatched:
-        new_row_unmatched = []
-        for i in range(gram.__len__()):
-            if i not in matched_is:
-                new_row_unmatched.append(row[i])
-        new_unmatched.append(new_row_unmatched)
-    return new_matched, new_unmatched
+def train_validation_test_split(gram, gtruths, validation_indices, test_indices):
+    length = len(gram)
+    train_validation_matrix = [[gram[i][j]
+                                for j in range(length) if j not in test_indices]
+                               for i in range(length) if i not in test_indices]
+    test_matrix = [[gram[i][j]
+                    for j in range(length) if j not in test_indices]
+                   for i in range(length) if i in test_indices]
 
-def train_validation_test_split(gtruths, validation_indices, test_indices):
-    test_matrix, train_validation_matrix = separate_gram(gram, test_indices)
-    validation_matrix, train_matrix = separate_gram(train_validation_matrix,
-                                                    validation_indices)
+    train_matrix = [[gram[i][j]
+                     for j in range(length) if j not in np.r_[validation_indices, test_indices]]
+                    for i in range(length) if i not in np.r_[validation_indices, test_indices]]
+    validation_matrix = [[gram[i][j]
+                          for j in range(length) if j not in np.r_[validation_indices,
+                                                                   test_indices]]
+                         for i in range(length) if i in validation_indices]
 
     train_gtruths = [gtruths[i] for i in range(len(gtruths))
                      if i not in validation_indices and i not in test_indices]
@@ -74,6 +58,17 @@ def train_validation_test_split(gtruths, validation_indices, test_indices):
                                 if i not in test_indices]
     test_gtruths = [gtruths[i] for i in range(len(gtruths))
                     if i in test_indices]
+
+    print(np.array(train_validation_matrix).shape)
+    print(np.array(test_matrix).shape)
+    print(np.array(train_matrix).shape)
+    print(np.array(validation_matrix).shape)
+    
+    print(np.array(train_validation_gtruths).shape)
+    print(np.array(test_gtruths).shape)
+    print(np.array(train_gtruths).shape)
+    print(np.array(validation_gtruths).shape)
+    
     return test_matrix, train_validation_matrix, validation_matrix, train_matrix,\
         train_gtruths, validation_gtruths, train_validation_gtruths, test_gtruths
 
@@ -102,23 +97,24 @@ def tryout1hyperparameter(cost, train, train_gtruths, validation_or_test, v_or_t
     return (roc_auc_score, f1_score)
 
 def optimizehyperparameter(costs, # [C]
+                           gram,
+                           gtruths,
                            validation_indices,
                            test_indices):
     error_to_hyperparameters = {}
     test_matrix, train_validation_matrix, validation_matrix, train_matrix,\
         train_gtruths, validation_gtruths, train_validation_gtruths, test_gtruths\
-        = train_validation_test_split(gtruths, validation_indices, test_indices)
+        = train_validation_test_split(gram, gtruths, validation_indices, test_indices)
     for cost in costs:
         error_to_hyperparameters[tryout1hyperparameter(cost, train_matrix,
                                                        train_gtruths,
                                                        validation_matrix,
-                                                       validation_gtruths)] = (sigma,
-                                                                               cost)
+                                                       validation_gtruths)] = (cost)
     #/* indices in the gram matrix is passed to the function to indicate the split. */
-    best_sigma, best_cost = error_to_hyperparameters[max(list(error_to_hyperparameters.keys()))]
+    best_cost = error_to_hyperparameters[max(list(error_to_hyperparameters.keys()))]
     test_matrix, train_validation_matrix, validation_matrix, train_matrix,\
         train_gtruths, validation_gtruths, train_validation_gtruths, test_gtruths\
-        = train_validation_test_split(gtruths, validation_indices, test_indices)
+        = train_validation_test_split(gram, gtruths, validation_indices, test_indices)
     print("test")
     return tryout1hyperparameter(best_cost, train_validation_matrix,
                                  train_validation_gtruths, test_matrix, test_gtruths)
@@ -129,15 +125,23 @@ def crossvalidation(mat_file_names, costs):
         mat = io.loadmat(mat_file_name)
         gram = mat['gram']
         num_indices = len(gram)
+        indices = mat['indices']
 
-        test_indices = mat['dropped_indices_number']
-        validation_indices = []
+        test_indices = mat['dropped_indices_number'][0]
+        tr_and_v_indices = []
 
         for i in range(num_indices):
             if i not in test_indices:
-                validation_indices.append(i)
+                tr_and_v_indices.append(i)
+                
+        validation_indices = np.random.permutation(tr_and_v_indices)\
+                             [:(len(tr_and_v_indices)//9)]
+
+        gtruths = [convert_index_to_attributes(index)['ground_truth'] for index in indices]
 
         errors.append(optimizehyperparameter(costs,
+                                             gram,
+                                             gtruths,
                                              validation_indices,
                                              test_indices))
 
@@ -152,6 +156,8 @@ def crossvalidation(mat_file_names, costs):
 def main():
     config_json_file = sys.argv[1]
     config_dict = json.load(open(config_json_file, 'r'))
+    global dataset_type
+    dataset_type = config_dict['dataset_type']
 
     data_dir = config_dict['data_dir']
     l2regularization_costs = config_dict['l2regularization_costs']
