01e2a06b0ec53b324cd162b24c92877fdb4b6f21
Zoltán Ádám Milacski <srph25@gmail.com>
2017-06-24 15:37:05 +0200

    Added docstring

--- gak_gram_matrix_completion/mean_squared_error_of_dropped_elements.py
+++ gak_gram_matrix_completion/mean_squared_error_of_dropped_elements.py
@@ -2,6 +2,19 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 
 def mean_squared_error_of_dropped_elements(m1, m2, elements):
+    """Compute masked Mean Squared Error.
+    Take subset of elements as vector, compute norm of difference, divided by subset size
+    
+    :param m1: True matrix
+    :param m2: Estimated matrix
+    :param elements: Subset indices
+    :type m1: np.ndarray
+    :type m2: np.ndarray
+    :type elements: list of 2-tuples
+    :returns: Mean Squared Error
+    :rtype: float
+    """
+    
     m1 = np.array(m1)
     m2 = np.array(m2)
     assert m1.shape == m2.shape
@@ -12,6 +25,19 @@ def mean_squared_error_of_dropped_elements(m1, m2, elements):
     return mse
 
 def relative_error(m1, m2, elements):
+    """Compute masked Relative Squared Error.
+    Take subset of elements as vector, compute norm of difference, divided by norm
+    
+    :param m1: True matrix
+    :param m2: Estimated matrix
+    :param elements: Subset indices
+    :type m1: np.ndarray
+    :type m2: np.ndarray
+    :type elements: list of 2-tuples
+    :returns: Relative Squared Error
+    :rtype: float
+    """
+
     # m1 is ground truth
     m1 = np.array(m1)
     m2 = np.array(m2)
