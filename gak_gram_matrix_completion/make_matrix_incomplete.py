c50b5f77a7343537eb05ba6dab787085f8397ed8
Zoltán Ádám Milacski <srph25@gmail.com>
2017-06-25 14:49:34 +0200

    make_matrix_incomplete refactor

--- gak_gram_matrix_completion/make_matrix_incomplete.py
+++ gak_gram_matrix_completion/make_matrix_incomplete.py
@@ -2,70 +2,123 @@ import random
 import numpy as np
 import copy
 
-def make_matrix_incomplete(seed, similarities, incomplete_percentage):
+def drop_gram_random(seed, gram, percent):
+    """Drop elements randomly from Gram matrix.
+
+    :param seed: Random seed for reproducibility
+    :param gram: Gram matrix to be dropped
+    :param percent: Percent of matrix elements to drop
+    :type seed: int
+    :type gram: list of lists
+    :type percent: int
+    :returns: Dropped version of Gram matrix, dropped indices
+    :rtype: list of lists, list of tuples
+    """
+    
     random.seed(seed)
         
-    incomplete_similarities = []
-    dropped_elements = []
+    gram_drop = []
+    indices_drop = []
 
-    half_indices = [(i, j) for i in range(len(similarities))
-                   for j in range(i + 1, len(similarities[0]))]
+    half_indices = [(i, j) for i in range(len(gram))
+                   for j in range(i + 1, len(gram[0]))]
     permutated_half_indices = np.random.permutation(half_indices)
-    num_to_drop = int(len(permutated_half_indices) * incomplete_percentage * 0.01)
-    dropped_elements = permutated_half_indices[:num_to_drop]
+    num_to_drop = int(len(permutated_half_indices) * percent * 0.01)
+    indices_drop = permutated_half_indices[:num_to_drop]
 
-    incomplete_similarities = copy.deepcopy(similarities)
-    for i, j in dropped_elements:
-        incomplete_similarities[i][j] = np.nan
-        incomplete_similarities[j][i] = np.nan
+    gram_drop = copy.deepcopy(gram)
+    for i, j in indices_drop:
+        gram_drop[i][j] = np.nan
+        gram_drop[j][i] = np.nan
         
-    print("Incomplete matrix is provided.")
-    return incomplete_similarities, dropped_elements
+    return gram_drop, indices_drop
 
-def drop_samples(similarities, indices):
-    dropped_elements = []
+def drop_one_sample(gram, index):
+    """Drop ith row and ith column from Gram matrix.
+
+    :param gram: Gram matrix to be dropped
+    :param index: Row and column index to be dropped
+    :type gram: list of lists
+    :type index: int
+    :returns: Dropped version of Gram matrix, dropped indices
+    :rtype: list of lists, list of tuples
+    """
+
+    gram_drop = copy.deepcopy(gram)
+    indices_drop = []
+    for i in range(len(gram)):
+        gram_drop[i][index] = np.nan
+        indices_drop.append((i, index))
+    for j in range(len(gram[0])):
+        gram_drop[index][j] = np.nan
+        indices_drop.append((index, j))
+
+    return gram_drop, indices_drop
+
+def drop_samples(gram, indices):
+    """Drop multiple rows and columns from Gram matrix.
+
+    :param gram: Gram matrix to be dropped
+    :param indices: Row and column indices to be dropped
+    :type gram: list of lists
+    :type indices: list of ints
+    :returns: Dropped version of Gram matrix, dropped indices
+    :rtype: list of lists, list of tuples
+    """
+
+    gram_drop = copy.deepcopy(gram)
+    indices_drop = []
     for index in indices:
-        similarities, dropped_elements_ = drop_one_sample(similarities, index)
-        dropped_elements += dropped_elements_
-    return similarities, dropped_elements
+        gram_drop, indices_drop_i = drop_one_sample(gram_drop, index)
+        indices_drop += indices_drop_i
+    return gram_drop, indices_drop
 
-def drop_one_sample(similarities, index):
-    incomplete_similarities = copy.deepcopy(similarities)
-    dropped_elements = []
-    for i in range(len(similarities)):
-        incomplete_similarities[i][index] = np.nan
-        dropped_elements.append((i, index))
-    for j in range(len(similarities[0])):
-        incomplete_similarities[index][j] = np.nan
-        dropped_elements.append((index, j))
 
-    print("Incomplete matrix is provided.")
-    return incomplete_similarities, dropped_elements
+def drop_in_sample_random(gram, index, percent):
+    """Drop random part of ith row and ith column from Gram matrix.
 
-def drop_randomly_in_samples(similarities, indices, loss_persentage):
-    dropped_elements = []
+    :param gram: Gram matrix to be dropped
+    :param index: Row and column indices to be dropped
+    :param percent: Percent of row and column to be dropped
+    :type gram: list of lists
+    :type index: list of ints
+    :type percent: int
+    :returns: Dropped version of Gram matrix, dropped indices
+    :rtype: list of lists, list of tuples
+    """
+    
+    length = len(gram)
+
+    permutated_indices = np.random.permutation(length)
+    num_to_drop = int(length * loss_persentage * 0.01)
+    subindices_drop = permutated_indices[:num_to_drop]
+
+    gram_drop = copy.deepcopy(gram)
+    indices_drop = []
+    for i in subindices_drop:
+        gram_drop[i][index] = np.nan
+        indices_drop.append((i, index))
+        gram_drop[index][i] = np.nan
+        indices_drop.append((index, i))
+
+    return gram_drop, indices_drop
+
+def drop_in_samples_random(gram, indices, percent):
+    """Drop random parts of multiple rows and columns from Gram matrix.
+
+    :param gram: Gram matrix to be dropped
+    :param indices: Row and column indices to be dropped
+    :type gram: list of lists
+    :type indices: list of ints
+    :returns: Dropped version of Gram matrix, dropped indices
+    :rtype: list of lists, list of tuples
+    """
+    
+    gram_drop = copy.deepcopy(gram)
+    incies_drop = []
     for index in indices:
-        similarities, dropped_elements_ = drop_randomly_in_a_sample(similarities,
-                                                                    index,
-                                                                    loss_persentage)
-        dropped_elements += dropped_elements_
-    return similarities, dropped_elements
+        gram_drop, indices_drop_i = drop_in_sample_random(similarities, index, percent)
+        indices_drop += indices_drop_i
+    return gram_drop, indices_drop
 
-def drop_randomly_in_a_sample(similarities, index, loss_persentage):
-    length = len(similarities)
-
-    permutated_indices = np.random.permutation([i for i in range(length)])
-    num_to_drop = length * loss_persentage // 100
-    indices_to_drop = permutated_indices[:num_to_drop]
-
-    incomplete_similarities = copy.deepcopy(similarities)
-    dropped_elements = []
-    for i in indices_to_drop:
-        incomplete_similarities[i][index] = np.nan
-        dropped_elements.append((i, index))
-        incomplete_similarities[index][i] = np.nan
-        dropped_elements.append((index, i))
-
-    return incomplete_similarities, dropped_elements
-        
     
