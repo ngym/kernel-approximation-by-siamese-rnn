85c92f42519d41c6156b7521114aa4a5b5925ea0
Zoltán Ádám Milacski <srph25@gmail.com>
2017-06-25 14:00:20 +0200

    gak refactor

--- gak_gram_matrix_completion/gak.py
+++ gak_gram_matrix_completion/gak.py
@@ -1,5 +1,5 @@
 import sys
-import TGA_python3_wrapper.global_align as ga
+import global_align as ga
 import numpy as np
 
 import scipy as sp
@@ -8,43 +8,34 @@ from scipy.io import wavfile
 from scipy import signal
 
 def gak(seq1, seq2, sigma, triangular):
-    #print(threading.get_ident())
+    """Triangular Global Alignment (TGA) kernel computation between two time series.
+    Meaningful similarity measure between time series
+    Computes exponential soft minimum of all valid alignments
+    May restrict the set of alignments with the triangular parameter
+    Wrapper for the code of Marco Cuturi and Adrien Gaidon: http://marcocuturi.net/GA.html
+
+    :param seq1: First time series
+    :param seq2: Second time series
+    :param sigma: TGA kernel scale parameter
+    :param triangular: TGA kernel band parameter
+    :type seq1: np.ndarray
+    :type seq2: np.ndarray
+    :type sigma: float
+    :type triangular: int
+    :returns: TGA kernel value
+    :rtype: float
+    """
+    
     if seq1 is seq2:
         return 1
     
-    T1 = seq1.__len__()
-    T2 = seq2.__len__()
-
-    #sigma = 0.5*(T1+T2)/2*np.sqrt((T1+T2)/2)
-    #sigma = 2 ** 0
-    #print("sigma: " + repr(sigma), end="  ")
-    
-    #triangular = (T1+T2) * 0.5
-
     val = ga.tga_dissimilarity(seq1, seq2, sigma, triangular)
     kval = np.exp(-val)
     return kval
 
-def read_and_resample(f, frequency):
-    mat_filename = f.replace(".wav", ("_freq" + str(frequency) + ".mat"))
-    print(mat_filename)
-    try:
-        mat = io.loadmat(mat_filename)
-        resampled_data = mat['resampled_data']
-        print("read from mat")
-    except:
-        rate, data = io.wavfile.read(f)
-        length = frequency * data.__len__() // rate
-        print("read from wav")
-        resampled_data = signal.resample(data, length)
-    return resampled_data
-
 def main():
-    f1 = sys.argv[1]
-    f2 = sys.argv[2]
-
-    seq1 = read_and_resample(f1, 100)
-    seq2 = read_and_resample(f2, 100)
+    seq1 = sys.argv[1]
+    seq2 = sys.argv[2]
 
     val = gak(seq1, seq2, 0.4, 500)
     print(val)
