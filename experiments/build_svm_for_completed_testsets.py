a01ba8a1e0c2bf18ec7b2dfe3ed262556df7a33e
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2017-06-29 22:51:24 +0200

    svm_for_completed_testsets

--- experiments/build_svm_for_completed_testsets.py
+++ experiments/build_svm_for_completed_testsets.py
@@ -23,24 +23,39 @@ def main():
         json.dump(json_dict, fd)
         fd.close()
     
-    experiment_dirs = ["/home/milacski/shota/USE_CASE_GAK_COMPLETION_1_VALIDATION",
-                       "/home/milacski/shota/USE_CASE_SOFTIMPUTE_COMPLETION_1_VALIDATION"]
-    for experiment_dir in experiment_dirs:
-        for dataset_type, direc, gram_file in [("6DMG", "6DMGupperChar/all/20/0", "gram_upperChar_all_sigma20.000_t1-t3_dropfrom0_RNN_LSTM.pkl"),
-                                               ("UCIauslan", "UCIauslan/12/0", "gram_UCIauslan_sigma12.000_dropfrom0_RNN_LSTM.pkl"),
-                                               ("UCIcharacter", "UCIcharacter/20/0", "gram_UCIcharacter_sigma20.000_dropfrom0_RNN_LSTM.pkl")]:
-            data_dir = os.path.join(experiment_dir, direc)
-            output_file = gram_file.replace('.pkl', '_svm_out.json')
-            json_dict = dict(dataset_type=dataset_type,
-                             data_dir=data_dir,
-                             completion_matrices_for_glob=[gram_file],
-                             l2regularization_costs=l2regularization_costs,
-                             output_file=output_file)
+    experiment_dir = "/home/milacski/shota/USE_CASE_GAK_COMPLETION_1_VALIDATION",
+    for dataset_type, direc, gram_file in [("6DMG", "6DMGupperChar/all/20/0", "gram_upperChar_all_sigma20.000_t1-t3_dropfrom0_RNN_LSTM.pkl"),
+                                           ("UCIauslan", "UCIauslan/12/0", "gram_UCIauslan_sigma12.000_dropfrom0_RNN_LSTM.pkl"),
+                                           ("UCIcharacter", "UCIcharacter/20/0", "gram_UCIcharacter_sigma20.000_dropfrom0_RNN_LSTM.pkl")]:
+        data_dir = os.path.join(experiment_dir, direc)
+        output_file = gram_file.replace('.pkl', '_svm_out.json')
+        json_dict = dict(dataset_type=dataset_type,
+                         data_dir=data_dir,
+                         completion_matrices_for_glob=[gram_file],
+                         l2regularization_costs=l2regularization_costs,
+                         output_file=output_file)
+        
+        json_file_name = os.path.join(data_dir, "svm_for_completed_testsets.json")
+        fd = open(json_file_name, "w")
+        json.dump(json_dict, fd)
+        fd.close()
     
-            json_file_name = os.path.join(data_dir, "svm_for_completed_testsets.json")
-            fd = open(json_file_name, "w")
-            json.dump(json_dict, fd)
-            fd.close()
+    experiment_dir = "/home/milacski/shota/USE_CASE_SOFTIMPUTE_COMPLETION_1_VALIDATION"]
+    for dataset_type, direc, gram_file in [("6DMG", "6DMGupperChar/all/0", "gram_upperChar_all_sigma20.000_t1-t3_dropfrom0_RNN_LSTM.pkl"),
+                                           ("UCIauslan", "UCIauslan/0", "gram_UCIauslan_sigma12.000_dropfrom0_RNN_LSTM.pkl"),
+                                           ("UCIcharacter", "UCIcharacter/0", "gram_UCIcharacter_sigma20.000_dropfrom0_RNN_LSTM.pkl")]:
+        data_dir = os.path.join(experiment_dir, direc)
+        output_file = gram_file.replace('.pkl', '_svm_out.json')
+        json_dict = dict(dataset_type=dataset_type,
+                         data_dir=data_dir,
+                         completion_matrices_for_glob=[gram_file],
+                         l2regularization_costs=l2regularization_costs,
+                         output_file=output_file)
+        
+        json_file_name = os.path.join(data_dir, "svm_for_completed_testsets.json")
+        fd = open(json_file_name, "w")
+        json.dump(json_dict, fd)
+        fd.close()
     
 
 if __name__ == '__main__':
