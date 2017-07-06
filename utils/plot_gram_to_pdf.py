3e90949b5d0e0f7ce68d5ea3d7730731377c7f4e
László Kopácsi <laszlo.kopacsi@gmail.com>
2017-07-06 13:49:01 +0200

    Refactor pdf plotting

--- utils/plot_gram_to_pdf.py
+++ utils/plot_gram_to_pdf.py
@@ -1,19 +1,20 @@
-import sys
+import sys, os
+from collections import OrderedDict
+
 import numpy as np
-from scipy import io
-
-from functools import reduce
-
 import matplotlib
-
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-import pickle
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from utils import file_utils
 
 
-def plot_gram_to_pdf(file_name, gram, files, separators, labels,
-                     sigma, drop_percent, rotate_vertically=True, dataset_name=""):
+def plot_gram_to_pdf(file_name, gram, files,
+                     separators, labels,
+                     dataset_name, title="",
+                     sigma=None, drop_percent=None,
+                     rotate_vertically=True):
     """Plot Gram matrix and save as pdf file with matplotlib.
 
     :param file_name: Output pdf file name
@@ -21,21 +22,22 @@ def plot_gram_to_pdf(file_name, gram, files, separators, labels,
     :param files: Filenames used for Global Alignment Kernel calcucation
     :param separators: Class sizes for label separators
     :param labels: Class names for labeling
+    :param dataset_name: Name of the dataset
+    :param title: Title of the plot
     :param sigma: Global Alignment Kernel sigma parameter
     :param drop_percent: Amount of Gram matrix elements removed
     :param rotate_vertically: If True then rotate the bottom labels vertically
-    :param dataset_name: Name of the dataset
     :type file_name: str
     :type gram: np.ndarray
     :type files: list of str
     :type separators: list of int
     :type labels: list of str
+    :type dataset_name: str
+    :type title: str
     :type sigma: float
     :type drop_percent: float
     :type rotate_vertically: bool
-    :type dataset_name: str
     """
-
     assert len(separators) == len(labels) - 1
 
     cmap = plt.get_cmap('bwr')
@@ -45,8 +47,7 @@ def plot_gram_to_pdf(file_name, gram, files, separators, labels,
     f1 = ax.imshow(gram,
                    interpolation='nearest',
                    cmap=cmap,
-                   vmax=1, vmin=0
-                   )
+                   vmax=1, vmin=0)
 
     # Colorbar on the right of the graph
     fig.colorbar(f1) #, ax=ax) #, shrink=0.9)
@@ -70,8 +71,6 @@ def plot_gram_to_pdf(file_name, gram, files, separators, labels,
         label = labels[i]
         x = -len(files)//100
         y = separators_[i] + 0.75 * (separators_[i+1] - separators_[i])
-        #y = np.mean([separators_[i], separators_[i+1]]) \
-        #    + len(files) // 75
         if all([len(l) == 1 for l in labels]):
             ha = "center"
             x = -len(files)//50
@@ -89,152 +88,86 @@ def plot_gram_to_pdf(file_name, gram, files, separators, labels,
                     xy=(0, 0), xytext=(x, y))
 
     # Title of the graph, displaying the sigma and the drop percent
-    titletext = dataset_name + " σ =" + str(sigma)  # + "    drop=" + drop_percent #+ "%\n "
+    titletext = dataset_name
+    if title != "":
+        titletext += ": " + title
+    if sigma is not None:
+        titletext += " σ =" + str(sigma)
+    if drop_percent is not None:
+        titletext += ", drop=" + str(drop_percent)
     ax.set_title(titletext,
                  horizontalalignment='center')
-
-    #fig.suptitle(titletext,
-    #             horizontalalignment='center')
 
     plt.savefig(file_name, format='pdf', dpi=1200)
 
 
-def main():
-    """Read .mat file, parse its metadata, plot Gram matrix and save as pdf file with matplotlib.
-    """
+def get_informations(dataset_type, sample_names):
+    def get_labels_and_separators(sample_names, dataset_type):
+        """Get labels and separators from sample names.
+        :param sample_names: A list of sample names.
+        :param dataset_type: 6DMGupperChar or UCIauslan or UCIcharacter.
+        :type sample_names: list of str
+        :type dataset_type: str
+        :return: List of labels and list of separators.
+        :rtype: list of str, list of int
+        """
+        # Lambdas to calculate labels from keys of the sequences
+        get_label = dict.fromkeys(["6DMG", "6DMGupperChar", "upperChar"], lambda fn: fn.split('/')[-1].split('_')[1])
+        get_label["UCIcharacter"] = lambda str: str[0]
+        get_label["UCIauslan"] = lambda fn: fn.split('/')[-1].split('-')[0]
 
-    filename = sys.argv[1]
-    if filename[-4:] == ".mat":
-        mat = io.loadmat(filename)
-        gram = mat['gram']
-        sample_names = mat['indices']
-        dropped_gram = mat['dropped_gram']
-        original_gram = mat['orig_gram']
-        sigma = float(filename.split("sigma")[1]
-                      .split("_")[0]
-                      .replace(".mat", ""))
-        if filename.find("loss") == -1:
-            drop_percent = "0%"
-        else:
-            drop_percent = filename.split("loss")[1].split("_")[0].replace(".mat", "") + "%"
-        filename_pdf = filename.replace(".mat", ".pdf")
-    elif filename[-4:] == ".pkl":
-        fd = open(filename, 'rb')
-        pkl = pickle.load(fd)
-        fd.close()
-        dataset_type = pkl['dataset_type']
-        gram_matrices = pkl['gram_matrices']
-        sample_names = pkl['sample_names']
-        if len(gram_matrices) == 1:
-            gram = gram_matrices[0]['gram_original']
-        else:
-            gram = gram_matrices[-1]['gram_completed_npsd']
-            dropped_gram = gram_matrices[-1]['dropped_gram']
-            original_gram = gram_matrices[-1]['orig_gram']
-        sigma = float(filename.split("sigma")[1]
-                      .split("_")[0]
-                      .replace(".pkl", ""))
-        if filename.find("loss") == -1:
-            drop_percent = "0%"
-        else:
-            drop_percent = filename.split("loss")[1].split("_")[0].replace(".pkl", "") + "%"
-        filename_pdf = filename.replace(".pkl", ".pdf")
-    else:
-        assert False
+        if dataset_type not in get_label:
+            assert False
 
-    dataset_name = ""
+        label_to_separator = OrderedDict()
+        for index, sample_name in enumerate(sample_names):
+            label = get_label[dataset_type](sample_name)
+            if label not in label_to_separator:
+                label_to_separator[label] = index
+        labels = list(label_to_separator.keys())
+        separators = list(label_to_separator.values())[1:]
+        return labels, separators
+
+    dataset_name = dataset_type
     rotate = True
-    if filename.find("audioset") != -1:
-        dataset_name = "audioset"
-        i = 0
-        while sample_names[i].find("Bark") != -1:
-            i += 1
-            separators = [i]
-        labels = ['Bark', 'Meow']
-    elif filename.find("num") != -1:
-        i = 0
-        while sample_names[i].find("num_3") != -1:
-            i += 1
-            separators = [i]
-        labels = ['3', '4']
-    elif filename.find("upperChar") != -1:
-        dataset_name = "upperChar"
+    if dataset_type in {"6DMG", "6DMGupperChar", "upperChar"}:
+        dataset_name = "6DMG"
         rotate = False
-        labels = ["A", "B", "C", "D", "E", "F", "G",
-                  "H", "I", "J", "K", "L", "M", "N",
-                  "O", "P", "Q", "R", "S", "T", "U",
-                  "V", "W", "X", "Y", "Z"]
-        separators = []
-        for label in labels[:-1]:
-            i = 0
-            while sample_names[i].find("upper_" + label) == -1:
-                i += 1
-            while sample_names[i].find("upper_" + label) != -1:
-                i += 1
-            separators.append(i)
-    elif filename.find("UCIcharacter") != -1:
-        dataset_name = "UCIcharacter"
+    elif dataset_type == "UCIcharacter":
         rotate = False
-        labels = []
-        for f in sample_names:
-            l = f[0]
-            if l not in labels:
-                labels.append(l)
-        separators = []
-        for l in labels[:-1]:
-            i = 0
-            while sample_names[i].split('/')[-1].find(l) == -1:
-                i += 1
-            while sample_names[i].split('/')[-1].find(l) != -1:
-                i += 1
-            separators.append(i)
-    elif filename.find("UCIauslan") != -1 or filename.find("UCItctodd") != -1:
-        dataset_name = "UCIauslan"
-        labels = []
-        for f in sample_names:
-            l = f.split('/')[-1].split('-')[0]
-            if l not in labels:
-                labels.append(l)
-        separators = []
-        for l in labels[:-1]:
-            i = 0
-            while sample_names[i].split('/')[-1].split('-')[0] != l:
-                i += 1
-            while sample_names[i].split('/')[-1].split('-')[0] == l:
-                i += 1
-            separators.append(i)
+
+    labels, separators = get_labels_and_separators(sample_names, dataset_type)
+
+    return labels, separators, dataset_name, rotate
+
+
+def main():
+    """Read .pkl file, parse its metadata, plot Gram matrix and save as pdf file with matplotlib.
+    """
+    filename = os.path.abspath(sys.argv[1])
+    title = sys.argv[2] if len(sys.argv) > 2 else ""
+
+    assert filename[-4:] == ".pkl"
+
+    pkl = file_utils.load_pickle(filename)
+    dataset_type = pkl['dataset_type']
+    gram_matrices = pkl['gram_matrices']
+    sample_names = pkl['sample_names']
+
+    labels, separators, dataset_name, rotate = get_informations(dataset_type, sample_names)
+
+    if len(gram_matrices) == 1:
+        matrices = gram_matrices[0]
     else:
-        assert False
+        matrices = gram_matrices[-1]
 
-    # OUTPUT
-    plot_gram_to_pdf(filename_pdf,
-                     gram,
-                     sample_names,
-                     separators,
-                     labels,
-                     sigma,
-                     drop_percent,
-                     rotate,
-                     dataset_name)
-    """
-    filename_pdf_dropped = filename.replace(".mat", "_dropped.pdf")
-    plot_gram_to_pdf(filename_pdf_dropped,
-                     mat['dropped_gram'],
-                     sample_names,
-                     separators,
-                     labels,
-                     sigma,
-                     drop_percent)
-
-    filename_pdf_orig = filename.replace(".mat", "_orig.pdf")
-    plot_gram_to_pdf(filename_pdf_orig,
-                     mat['orig_gram'],
-                     sample_names,
-                     separators,
-                     labels,
-                     sigma,
-                     drop_percent)
-    """
+    for key in matrices.keys():
+        filename_pdf = filename.replace(".pkl", key + ".pdf")
+        plot_title = title + " " + key.replace("_", " ")
+        plot_gram_to_pdf(filename_pdf, matrices[key], sample_names,
+                         separators, labels,
+                         dataset_name, title=plot_title,
+                         rotate_vertically=rotate)
 
 
 if __name__ == "__main__":
