5fde6a38a163dd03a3a6218fb0289f1f3e21a459
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2017-10-12 04:20:44 +0200

    debug

--- algorithms/siamese_rnn.py
+++ algorithms/siamese_rnn.py
@@ -167,7 +167,7 @@ class SiameseRnn(Rnn):
 
             # validation
             average_validation_loss = do_epoch("validation", epoch, epochs,
-                                               tr_indices, gram_drop, seqs, labels,
+                                               validation_indices, gram_drop, seqs, labels,
                                                loss_weight_ratio, loss_file)
 
 
