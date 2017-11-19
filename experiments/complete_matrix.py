4d8aa47a8699c5859c046c5ce83313d389d60fa2
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2017-11-19 03:43:31 +0100

    update fine_tuning settings

--- experiments/complete_matrix.py
+++ experiments/complete_matrix.py
@@ -206,7 +206,7 @@ def run(pickle_or_hdf5_location, dataset_location, fold_count, fold_to_drop,
                                                       params['siamese_arms_activation'],
                                                       classify_one_by_all=params['classify_one_by_all'],
                                                       target_label=params['target_label'],
-                                                      trained_modelfile_hdf5=params['trained_modelfile_hdf5=params'])
+                                                      trained_modelfile_hdf5=params['trained_modelfile_hdf5'])
         action = "SiameseRNN"
     else:
         assert False
