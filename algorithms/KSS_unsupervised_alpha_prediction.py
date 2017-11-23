aa48239a48ac1a260bf5a22d48734fb147559d47
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2017-11-23 01:39:00 +0100

    try

--- algorithms/KSS_unsupervised_alpha_prediction.py
+++ algorithms/KSS_unsupervised_alpha_prediction.py
@@ -600,7 +600,7 @@ class GroupSoftThresholdingLayer(Layer):
 class SoftThresholdingActivation(Layer):
     def __init__(self, **kwargs):
         super(SoftThresholdingActivation, self).__init__(**kwargs)
-        self.threshold = 0.01
+        self.threshold = 0.001
     def call(self, inputs):
         return K.sign(inputs) * K.relu(K.abs(inputs) - self.threshold)
     def get_config(self):
