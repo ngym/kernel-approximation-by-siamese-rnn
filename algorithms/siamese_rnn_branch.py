4f70a81299521af7c9d0ef3894508ecc7a3203fa
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2017-12-29 09:39:52 +0100

    forgot to add

---
+++ algorithms/siamese_rnn_branch.py
@@ -0,0 +1,30 @@
+from siamese_rnn import SiameseRNN
+
+class SiameseRNNBranch():
+    def __init__(self, input_shape, pad_value, rnn_units, dense_units,
+                 rnn, dropout, implementation, bidirectional, batchnormalization,
+                 loss_function, siamese_joint_method, siamese_arms_activation='linear'):
+        siamese_model = SiameseRNN(input_shape, pad_value, rnn_units, dense_units,
+                                   rnn, dropout, implementation, bidirectional, batchnormalization,
+                                   loss_function, siamese_joint_method, siamese_arms_activation='linear'):
+        
+        """
+        print(K.get_value(model.layers[2].layers[1].weights[0])[0])
+        model.load_weights()
+        print(K.get_value(model.layers[2].layers[1].weights[0])[0])
+        model = Model(input_a, processed_a)
+        print(K.get_value(model.layers[1].layers[1].weights[0])[0])
+        """
+        print(K.get_value(siamese_model.layers[2].layers[1].weights[0])[0])
+        siamese_model.load_weights(trained_modelfile_hdf5)
+        print(K.get_value(siamese_model.layers[2].layers[1].weights[0])[0])
+        model = Model(siamese_model.input_a, siamese_model.processed_a)
+        print(K.get_value(model.layers[1].layers[1].weights[0])[0])
+        model.compile(loss=loss_function, loss_weights=loss_weights, optimizer=optimizer)
+         
+        self.model = model
+    def predict(self, seqs):
+        train_feature = self.model.predict(seqs)
+        return train_feature
+
+        
