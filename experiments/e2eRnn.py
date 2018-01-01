f36d934d26941e0346da73367d10b35d2b9d6c07
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2018-01-01 20:19:48 +0100

    forgot to add

--- experiments/e2eRnn.py
+++ experiments/e2eRnn.py
@@ -159,7 +159,7 @@ def run(dataset_type, dataset_location, fold_count, fold_to_drop,
     input_ = Input(shape=input_shape)
     base_network = rnn_.create_RNN_base_network()
     hidden = base_network(input_)
-    output_ = hidden.add(Dense(len(set(list(key_to_str.values()))), activation='softmax'))
+    output_ = Dense(len(set(list(key_to_str.values()))), activation='softmax')(hidden)
     model = Model(input_, output_)
     optimizer = RMSprop(clipnorm=1.)
     model.compile(loss='categirical_crossentropy', optimizer=optimizer)
