4fd4f4d5120503802a7da1cf162784a97004a469
永山 翔太 <kurosagi@sfc.wide.ad.jp>
2018-02-12 20:44:10 +0800

    fix

--- experiments/compare_grams.py
+++ experiments/compare_grams.py
@@ -9,7 +9,7 @@ def main():
     loaded_data1 = file_utils.load_hdf5(file1)
     loaded_data2 = file_utils.load_hdf5(file2)
     assert loaded_data1['dropped_indices'] == loaded_data2['dropped_indices']
-    dropped_indices = loaded_data1['dropped_indices']
+    dropped_indices = loaded_data1['dropped_indices'][-1]
     dropped_elements = [(i, j) for i in dropped_indices for j in dropped_indices]
     
     gram1 = loaded_data1['gram_matrices'][-1]['completed_npsd']
@@ -17,7 +17,7 @@ def main():
     
     errs = calculate_errors(gram1, gram2, dropped_elements)
     print("mse:%f, mse_dropped:%f, mae:%f, mae_dropped:%f, re:%f, re_dropped:%f" % errs)
-
+    
 
 if __name__ == "__main__":
     main()
